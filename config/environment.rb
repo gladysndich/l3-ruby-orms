6cb8971207fe7ab11dd4f2d2664f3a8fb2b8b308
gladysndich <itsgladysbrian@gmail.com>
2022-08-24 14:19:00 +0300

    Competed TODO

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,17 @@ require_relative '../lib/student'
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+
+#CREATE TABLE
+Student.create_table
+
+#Create a new student
+gladys = Student.new(name: "Gladys", age: 25)
+#add student to database
+gladys.add_to_db
+pp Student.show_all
+gladys.name = "Another NAME"
+gladys.age = 22
+gladys.update_student
+
+pp Student.show_all
